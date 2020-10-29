4fc7e08093198b65f802b459fb6ac95b38911c93
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-10-29 15:15:55 +0500

    Update App.js

--- AwesomeProject1/App.js
+++ AwesomeProject1/App.js
@@ -6,20 +6,17 @@ const styles = StyleSheet.create({
     color: 'blue',
     fontWeight: 'bold',
     fontSize: 30,
-  },
-  red: {
-    color: 'red',
-  },
+    textAlign:'center'
+  }
 });
 
 export default class LotsOfStyles extends Component {
   render() {
     return (
       <View>
-        <Text style={styles.red}>just red</Text>
-        <Text style={styles.bigBlue}>just bigBlue</Text>
-        <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
-        <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
+<Text style={styles.bigBlue}>
+  UTech
+</Text>
       </View>
     );
   }
